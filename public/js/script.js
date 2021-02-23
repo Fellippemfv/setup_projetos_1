c592be4386004b379ae30517f772e110089c4b0c
Fellippe Matheus <Fellippe.matheus2014@gmail.com>
2021-02-23 12:47:12 -0300

    adicionando menu mobile e pequenas alterações no menu

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,4 @@
+//----------//-----------MODAL DE LOGIN------------//-----------//
 // Get the modal
 var modal = document.getElementById('id01');
 
@@ -5,5 +6,16 @@ var modal = document.getElementById('id01');
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
+  }
+}
+
+//----------//-----------MENU MOBILE------------//-----------//
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
   }
 }
