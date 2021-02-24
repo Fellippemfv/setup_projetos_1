e49fe99bcf2629af34a2c6e6673fa6583a6739a4
Fellippe Matheus <Fellippe.matheus2014@gmail.com>
2021-02-23 22:05:11 -0300

    adicionando ultimas funcionalidades ao header usando javascript, e detalhes do mobile

--- public/js/script.js
+++ public/js/script.js
@@ -29,3 +29,15 @@ function myFunction2() {
   }
 }
 
+//----------//-----------MODAL DE PESQUISA MOBILE------------//-----------//
+// Open the full screen search box
+function openSearch() {
+  document.getElementById("myOverlay").style.display = "block";
+}
+
+// Close the full screen search box
+function closeSearch() {
+  document.getElementById("myOverlay").style.display = "none";
+}
+
+//----------//-----------EFEITO NO MENU------------//-----------//
