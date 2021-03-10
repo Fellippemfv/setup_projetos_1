14fda685ea3ece1071b4b55f8c1f64e755055eb6
Fellippe Matheus <Fellippe.matheus2014@gmail.com>
2021-03-10 13:46:15 -0300

    adicionando funcionalidade de upload de arquivos

--- src/app/routes/User.js
+++ src/app/routes/User.js
@@ -4,6 +4,10 @@ import authUser from "../middlewares/authUser"
 const routes = new Router();
 
 
+import multer from 'multer';
+import multerConfig from '../config/multer';
+const upload = multer(multerConfig);
+
 routes.get("/", User.getIndex);//PAGINA INICIAL DO BLOG
 
 routes.get("/user/:id", User.getProfileUser);//PAGINA DE UM USUÁRIO//falta
@@ -11,6 +15,12 @@ routes.get("/user/:id", User.getProfileUser);//PAGINA DE UM USUÁRIO//falta
 
 routes.get("/myprofile",authUser.requireAuth, User.getMyProfile);//PAGINA DE EDITAR PERFIL DO USUÁRIO
 routes.post("/myprofile",authUser.requireAuth, User.myProfile);//PAGINA DE EDITAR PERFIL DO USUÁRIO
+
+
+routes.post('/files', authUser.requireAuth, upload.single('file'), (req, res) => {
+    return res.redirect("/myprofile")
+});  
+
 
 
 
