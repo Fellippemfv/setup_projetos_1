6f438dcabc1f97f664b28cb9922d4022936b14bc
Fellippe Matheus <Fellippe.matheus2014@gmail.com>
2021-03-17 11:55:12 -0300

    adicionando base do dashboard de admin

--- src/app/routes/admin.js
+++ src/app/routes/admin.js
@@ -14,12 +14,12 @@ routes.get("/dashboard/users/edit",authAdmin.requireAuth, Admin.getUsersEdit);//
 routes.get("/dashboard/articles",authAdmin.requireAuth, Admin.getArticles);//PAGINA DE LISTA DE ARTIGOS DO BLOG
 routes.get("/dashboard/articles/deleted",authAdmin.requireAuth, Admin.getArticlesDeleted);//PAGINA DE LISTA DE ARTIGOS DO BLOG
 routes.get("/dashboard/articles/edit",authAdmin.requireAuth, Admin.getArticlesEdit);//PAGINA DE EDITAR ARTIGOS DO BLOG
-//routes.get("/dashboard/articles/new",authAdmin.requireAuth, Admin.getArticlesNew);//PAGINA DE CRIAR ARTIGOS DO BLOG
+routes.get("/dashboard/articles/new",authAdmin.requireAuth, Admin.getArticlesNew);//PAGINA DE CRIAR ARTIGOS DO BLOG
 
 routes.get("/dashboard/categories",authAdmin.requireAuth, Admin.getCategories);//PAGINA DE LISTA DE CATEGORIAS DO BLOG
 routes.get("/dashboard/categories/deleted",authAdmin.requireAuth, Admin.getCategoriesDeleted);//PAGINA DE LISTA DE ARTIGOS DO BLOG
 routes.get("/dashboard/categories/edit",authAdmin.requireAuth, Admin.getCategoriesEdit);//PAGINA DE EDITAR CATEGORIAS DO BLOG
-//routes.get("/dashboard/categories/new",authAdmin.requireAuth, Admin.getCategoriesNew);//PAGINA DE CRIAR CATEGORIAS DO BLOG
+routes.get("/dashboard/categories/new",authAdmin.requireAuth, Admin.getCategoriesNew);//PAGINA DE CRIAR CATEGORIAS DO BLOG
 
 
 export default routes;
